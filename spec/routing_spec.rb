cb00e4bac6a3365f0df0507b55b5ff96bcc4eedf
carladrago <carladrago@gmail.com>
2013-07-17 17:39:42 +0200

    added to the get request test and some examples in comments

--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -1,3 +1,5 @@
+require 'sinatra'
+
 describe "http methods" do 
 	# these tests should include 
 	# get, post, delete, head, put, options (all part of rfc2616)
@@ -5,9 +7,19 @@ describe "http methods" do
 	# link, unlink
 
 	describe "get" do
-		it "should send the correct response" do
+		it "should return 200 as a status" do
 
-			expect response.body == "Hello World"
+			#context returning full rack response [200, {}, ["body"]]
+			#context returning the body as a string "Hello World!"
+			#context returning the body as an array ["a", "b", "c"]
+			#context returning the body as an io object File.open("source_file")
+			app = Sinatra.new do 
+				get '/' do
+					[200, {}, 'Hello World']
+				end
+			end
+			response = app.call 'REQUEST_METHOD' => 'GET', 'rack.input' => ''
+			expect(response[0]).to eq 200
 		end
 	end
 end
