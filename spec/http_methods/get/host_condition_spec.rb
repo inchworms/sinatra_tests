e7c711475f2f15c05cee4bf72a6ae914b4d2b9f4
carladrago <carladrago@gmail.com>
2013-07-24 12:44:44 +0200

    add host_condition_spec

--- spec/http_methods/get/host_condition_spec.rb
+++ spec/http_methods/get/host_condition_spec.rb
@@ -4,4 +4,39 @@ require 'sinatra'
 require 'stringio'
 require 'support/rack'
 
-#passes to the next route when host_name does not match
+describe "get request" do |variable|
+ 
+	it "passes to the next route when host_name does not match" do
+		app = Sinatra.new do
+			host_name 'example.com'
+      get '/foo' do 
+        'Hello World'
+      end
+    end
+
+    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/foo', 'rack.input' => ''
+    expect(response[0]).to be == 404
+
+    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/foo', 'HTTP_HOST' => 'example.com', 'rack.input' => ''
+    expect(response[0]).to be == 200
+    expect(response[2]).to be == ["Hello World"]
+   end
+end
+	
+	#old test	
+  #   mock_app {
+  #     host_name 'example.com'
+  #     get '/foo' do
+  #       'Hello World'
+  #     end
+  #   }
+  #   get '/foo'
+  #   assert not_found?
+
+  #   get '/foo', {}, { 'HTTP_HOST' => 'example.com' }
+  #   assert_equal 200, status
+  #   assert_equal 'Hello World', body
+  
+
+
+
