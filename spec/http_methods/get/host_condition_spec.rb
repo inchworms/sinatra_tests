20228d77f94678ba6f9e2cf2feccab8ea4c04dd9
carladrago <carladrago@gmail.com>
2013-07-24 12:46:57 +0200

    added context to host_condition_spec file

--- spec/http_methods/get/host_condition_spec.rb
+++ spec/http_methods/get/host_condition_spec.rb
@@ -4,23 +4,26 @@ require 'sinatra'
 require 'stringio'
 require 'support/rack'
 
-describe "get request" do |variable|
+describe "get request" do
+
+	context "host_condition" do 
  
-	it "passes to the next route when host_name does not match" do
-		app = Sinatra.new do
-			host_name 'example.com'
-      get '/foo' do 
-        'Hello World'
-      end
-    end
+		it "passes to the next route when host_name does not match" do
+			app = Sinatra.new do
+				host_name 'example.com'
+	      get '/foo' do 
+	        'Hello World'
+	      end
+	    end
 
-    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/foo', 'rack.input' => ''
-    expect(response[0]).to be == 404
+	    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/foo', 'rack.input' => ''
+	    expect(response[0]).to be == 404
 
-    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/foo', 'HTTP_HOST' => 'example.com', 'rack.input' => ''
-    expect(response[0]).to be == 200
-    expect(response[2]).to be == ["Hello World"]
-   end
+	    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/foo', 'HTTP_HOST' => 'example.com', 'rack.input' => ''
+	    expect(response[0]).to be == 200
+	    expect(response[2]).to be == ["Hello World"]
+	  end
+	end
 end
 	
 	#old test	
