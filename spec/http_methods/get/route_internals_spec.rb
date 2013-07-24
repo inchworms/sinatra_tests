3845c46f13d3ea42f5b2a046aa66796be27fd49a
carladrago <carladrago@gmail.com>
2013-07-24 14:20:49 +0200

    commented out -returns route signature- test as it is no longer a requirement

---
+++ spec/http_methods/get/route_internals_spec.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+require 'sinatra'
+require 'stringio'
+require 'support/rack'
+
+describe "get request" do
+
+	context "route internals" do
+
+		#it 'returns the route signature' 
+
+	  it "sets env['sinatra.route'] to the matched route" do
+	  	app = Sinatra.new do 
+	  		after do 
+	  			expect(response[1][env]).to be == ["sinatra.route"]
+	  		end
+	  		get '/users/:id/status' do
+	  			'ok'
+	  		end
+	  	end
+	  	response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/users/1/status', 'rack.input' => ''
+	   end
+
+ end
+end
+
+ 
