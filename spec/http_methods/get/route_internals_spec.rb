cf10306bf4908645c1817ece035ed03a1cd55bb0
carladrago <carladrago@gmail.com>
2013-07-25 10:59:37 +0200

    non-working attempts with route internals

--- spec/http_methods/get/route_internals_spec.rb
+++ spec/http_methods/get/route_internals_spec.rb
@@ -7,18 +7,37 @@ describe "GET route internals" do
 	it 'returns the route signature'
 
   it "sets env['sinatra.route'] to the matched route" do
+
+    verifier = Proc.new { |params| 
+        expect(request.env['sinatra.route']).to be == ["users/:id/status"]    
+      }
+
   	app = Sinatra.new do 
   		after do 
-  			expect(response[1][env]).to be == ["sinatra.route"]
+  			verifier.call(params)
   		end
   		get '/users/:id/status' do
-  			'ok'
+  			[201, {} ["ok"]]
   		end
   	end
   	response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/users/1/status', 'rack.input' => ''
-   end
+  end
 
 end
 
 
+# --------------------------------------------------
+# old test
+ # it "sets env['sinatra.route'] to the matched route" do
+ #    mock_app do
+ #      after do
+ #        assert_equal 'GET /users/:id/status', env['sinatra.route']
+ #      end
+ #      get('/users/:id/status') { 'ok' }
+ #    end
+ #    get '/users/1/status'
+ #  end
+ #--------------------
+
+ 
  
