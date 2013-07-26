c6c4f57d21b495b000079886951ac209d884f741
carladrago <carladrago@gmail.com>
2013-07-26 16:45:25 +0200

    add route flow - no subsequent route match transitions to 404 and sets x-cascade header when passed

--- spec/http_methods/get/route_flow_spec.rb
+++ spec/http_methods/get/route_flow_spec.rb
@@ -4,36 +4,58 @@ require 'spec_helper'
 
 describe 'GET route flow' do
 
-	it 'returns response immediately on halt'
-	it 'halts with a response tuple'
-	it 'halts with an array of strings'
-	it 'sets response.status with halt'
-	it 'transitions to the next matching route on pass'
-	it 'transitions to 404 when passed and no subsequent route matches'
-	it 'transitions to 404 and sets X-Cascade header when passed and no subsequent route matches'
-	
+  it 'returns response immediately on halt'
+  it 'halts with a response tuple'
+  it 'halts with an array of strings'
+  it 'sets response.status with halt'
+  it 'transitions to the next matching route on pass'
+  
+  #it 'transitions to 404 when passed and no subsequent route matches' 
+  #this has been subsumed into test below...
 
-	
-	context 'optional blocks' do
+ 
+  
+  context 'no subsequent route matches' do
+   
+  	let(:app) do
+      Sinatra.new do
+        get('/:foo') do
+          pass
+            'Hello Foo'
+        end
+
+        get('/bar') do
+          'Hello Bar'
+        end
+      end
+    end
+
+    let(:response) { get '/foo' }
+    it('transitions to 404 when passed') { expect(response.status).to be == 404 }
+    it('sets X-Cascade header when passed') { expect(response.headers['X-Cascade']).to be == 'pass' }
+  end
+
+  
+  context 'optional blocks' do
     let(:app) do
-    	Sinatra.new do
-	      get('/') do
-        	pass do
-          	'this'
-        	end
-        	'not this'
-      	end
+      Sinatra.new do
+        get('/') do
+          pass do
+            'this'
+          end
+          'not this'
+        end
       end
     end
     
 
     it 'uses optional block passed to pass as route block if no other route is found' do 
-    	expect(get('/').body).to be == "this"
+      expect(get('/').body).to be == "this"
     end
   end
 
 
-	it "matches routes defined in superclasses" do
+  it "matches routes defined in superclasses" do
     base = Class.new(Sinatra::Base)
     base.get('/foo') { 'foo in baseclass' }
 
@@ -48,16 +70,16 @@ describe 'GET route flow' do
     expect(response[2]).to be == ['bar in subclass'] 
 
   end
-	
+  
 
-	it 'matches routes in subclasses before superclasses' do
+  it 'matches routes in subclasses before superclasses' do
     base = Class.new(Sinatra::Base)
     base.get('/foo') { 'foo in baseclass' }
     base.get('/bar') { 'bar in baseclass' }
 
     app = Sinatra.new(base) do 
       get'/foo' do 
-      	'foo in subclass'
+        'foo in subclass'
       end
     end
 
@@ -68,47 +90,47 @@ describe 'GET route flow' do
     expect(response[2]).to be == ['bar in baseclass'] 
   end
 
-	context 'internal request' do
+  context 'internal request' do
     let(:app) do
-	    Sinatra.new do
-	      get('/foo') do
-	        status, headers, body = call env.merge("PATH_INFO" => '/bar')
-	        [201, headers, body.each.map(&:upcase)]
-	      end
+      Sinatra.new do
+        get('/foo') do
+          status, headers, body = call env.merge("PATH_INFO" => '/bar')
+          [201, headers, body.each.map(&:upcase)]
+        end
 
-	      get('/bar') do
-	        'bar'
-	      end
-	    end
-	  end
+        get('/bar') do
+          'bar'
+        end
+      end
+    end
 
     it 'allows using call to fire another request internally' do 
-    	expect(get('/foo').status).to be == 201
-    	expect(get('/foo').body).to be == "BAR"
+      expect(get('/foo').status).to be == 201
+      expect(get('/foo').body).to be == "BAR"
     end
 
   end
 
 
-	context 'routing middleware' do
+  context 'routing middleware' do
     middleware = Sinatra.new
     inner_app  = Sinatra.new { get('/foo') { 'hello' } }
 
     builder = Rack::Builder.new do
       use middleware 
       map('/test') do 
-      	run inner_app
+        run inner_app
       end     
     end
 
     let(:app) do
-    	builder.to_app
+      builder.to_app
     end
 
     it 'plays well with other routing middleware' do
-   		expect(get('/test/foo').body).to be == 'hello'
-    	expect(get('/test/foo').status).to be == 200
- 		end
+      expect(get('/test/foo').body).to be == 'hello'
+      expect(get('/test/foo').status).to be == 200
+    end
   end
 
 end
