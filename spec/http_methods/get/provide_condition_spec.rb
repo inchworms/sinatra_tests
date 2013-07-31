c1ce39b5b10d0c3d25029a0511f45a5d73d5c80c
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-07-31 14:44:10 +0200

    add provide_condition_spec.rb filters by current Content-Type

--- spec/http_methods/get/provide_condition_spec.rb
+++ spec/http_methods/get/provide_condition_spec.rb
@@ -216,19 +216,65 @@ describe 'GET provide conditions' do
 
 
 
-	it 'filters by current Content-Type'
-	it 'allows multiple mime types for accept header'
-	it 'respects user agent preferences for the content type'
-	it 'accepts generic types'
-	it 'prefers concrete over partly generic types'
-	it 'prefers concrete over fully generic types'
-	it 'prefers partly generic over fully generic types'
-	it 'respects quality with generic types'
-	it 'supplies a default quality of 1.0'
-	it 'orders types with equal quality by parameter count'
-	it 'ignores the quality parameter when ordering by parameter count'
-	it 'properly handles quoted strings in parameters'
-	it 'accepts both text/javascript and application/javascript for js'
-	it 'accepts both text/xml and application/xml for xml'
+  it 'filters by current Content-Type' do
+    app = Sinatra.new do
+      before('/txt') { content_type :txt }
+      get '*', :provides => :txt do
+        'txt'
+      end
+      before('/html') { content_type :html }
+      get '*', :provides => :html do
+        'html'
+      end
+    end
+    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/', 'HTTP_ACCEPT' => '*/*', 'rack.input' => ''
+    expect(response[0]).to be == 200
+    expect(response[1]['Content-Type']).to be == 'text/plain;charset=utf-8'
+    expect(response[2]).to be == ['txt']
+    # mock_app do
+    #   before('/txt') { content_type :txt }
+    #   get('*', :provides => :txt) { 'txt' }
+
+    #   before('/html') { content_type :html }
+    #   get('*', :provides => :html) { 'html' }
+    # end
+
+    # get '/', {}, { 'HTTP_ACCEPT' => '*/*' }
+    # assert ok?
+    # assert_equal 'text/plain;charset=utf-8', response.headers['Content-Type']
+    # assert_body 'txt'
+
+    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/txt', 'HTTP_ACCEPT' => 'text/plain', 'rack.input' => ''
+    expect(response[0]).to be == 200
+    expect(response[1]['Content-Type']).to be == 'text/plain;charset=utf-8'
+    expect(response[2]).to be == ['txt']
+    # get '/txt', {}, { 'HTTP_ACCEPT' => 'text/plain' }
+    # assert ok?
+    # assert_equal 'text/plain;charset=utf-8', response.headers['Content-Type']
+    # assert_body 'txt'
+
+    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/', 'HTTP_ACCEPT' => 'text/html', 'rack.input' => ''
+    expect(response[0]).to be == 200
+    expect(response[1]['Content-Type']).to be == 'text/html;charset=utf-8'
+    expect(response[2]).to be == ['html']
+    # get '/', {}, { 'HTTP_ACCEPT' => 'text/html' }
+    # assert ok?
+    # assert_equal 'text/html;charset=utf-8', response.headers['Content-Type']
+    # assert_body 'html'
+  end
+
+  it 'allows multiple mime types for accept header'
+  it 'respects user agent preferences for the content type'
+  it 'accepts generic types'
+  it 'prefers concrete over partly generic types'
+  it 'prefers concrete over fully generic types'
+  it 'prefers partly generic over fully generic types'
+  it 'respects quality with generic types'
+  it 'supplies a default quality of 1.0'
+  it 'orders types with equal quality by parameter count'
+  it 'ignores the quality parameter when ordering by parameter count'
+  it 'properly handles quoted strings in parameters'
+  it 'accepts both text/javascript and application/javascript for js'
+  it 'accepts both text/xml and application/xml for xml'
 
 end
