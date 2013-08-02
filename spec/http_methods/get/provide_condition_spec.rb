f3c2d2c7fc50d2c1795ae7557048c79eedec3ef5
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-08-02 12:49:02 +0200

    refactor: supplies a default quality of 1.0 context in provide_condition.rb

--- spec/http_methods/get/provide_condition_spec.rb
+++ spec/http_methods/get/provide_condition_spec.rb
@@ -353,14 +353,17 @@ describe 'GET provide conditions' do
     end
   end
 
-  it 'supplies a default quality of 1.0' do
-    app = Sinatra.new do
-      get '/', :provides => [:png, :html] do
-        content_type
-      end
+  context 'supplies a default quality of 1.0' do
+    let(:app) do
+      Sinatra.new do
+        get('/', :provides => [:png, :html]){ content_type }
+     end
     end
-    response = app.call 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/', 'HTTP_ACCEPT' => 'image/png;q=0.5, text/*', 'rack.input' => ''
-    expect(response[2]).to be == ['text/html;charset=utf-8']
+
+    context "when HTTP_ACCEPT = image/png;q=0.5, text/*" do
+      let(:response){ get '/', {}, {'HTTP_ACCEPT' => 'image/png;q=0.5, text/*'} }
+      it("prefers text/html"){ expect(response.body).to be == 'text/html;charset=utf-8' }
+    end
   end
 
   it 'orders types with equal quality by parameter count' do
