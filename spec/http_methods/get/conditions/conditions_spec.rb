c1a42979a056b764676fbca6bb8f72228cfc1275
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-08-19 12:06:48 +0200

    refactor condition_spec.rb

--- spec/http_methods/get/conditions/conditions_spec.rb
+++ spec/http_methods/get/conditions/conditions_spec.rb
@@ -9,14 +9,13 @@ describe "GET conditions" do
         condition do
           pass unless params[:foo] == 'bar'
         end
-        get('/:foo'){ 'Hello World' }
+        get('/:foo'){ }
       end
     end
 
     context "get /bar" do
       let(:response){ get '/bar' }
       it("returns expected status"){ expect(response.status).to be == 200 }
-      it("returns expected body"){ expect(response.body).to be == 'Hello World' }
     end
     
     context "get /foo" do
@@ -31,14 +30,13 @@ describe "GET conditions" do
         condition do
           params[:foo] == 'bar'
         end
-        get('/:foo'){ 'Hello World' }
+        get('/:foo'){ }
       end
     end
 
     context "/bar" do
       let(:response){ get '/bar' }
       it("returns expected status"){ expect(response.status).to be == 200 }
-      it("returns expected body"){ expect(response.body).to be == 'Hello World' }
     end
 
     context "/foo" do
@@ -53,13 +51,12 @@ describe "GET conditions" do
         condition do
           nil
         end
-        get('/:foo'){ 'Hello World' }
+        get('/:foo'){ }
       end
     end
 
    let(:response){ get '/bar' }
    it("returns expected status"){ expect(response.status).to be == 200 }
-   it("returns expected body"){ expect(response.body).to be == 'Hello World' }
   end
 
   context 'allows custom route-conditions to be set via route options' do
