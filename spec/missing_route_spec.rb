4cf0c01c6c5fb22ce08a36f769c2c7d1412d49ce
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-08-26 14:17:03 +0200

    refactor: GET missing routes in missing_route_spec.rb

--- spec/missing_route_spec.rb
+++ spec/missing_route_spec.rb
@@ -7,7 +7,7 @@ describe "GET missing routes" do
   let(:response) { get '/noroute' }
 
   it("sets X-Cascade header when no route satisfies the request") { expect(response.header['X-Cascade']).to be == 'pass' }
-  it("throws an 404") { expect(response.status).to be == 404 }
+  it("throws a 404 when no route satisfies the request") { expect(response.status).to be == 404 }
 
   it "does not set X-Cascade header when x_cascade has been disabled" do
     app.disable :x_cascade
