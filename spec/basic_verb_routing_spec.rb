e8fcdef6df2cd0ddc4d560f4318d0b3c8127c12d
carladrago <carladrago@gmail.com>
2013-08-27 11:54:56 +0200

    add link and unlink tests

--- spec/basic_verb_routing_spec.rb
+++ spec/basic_verb_routing_spec.rb
@@ -2,7 +2,6 @@
 
 require 'spec_helper'
 
-request_types = [:post, :put, :delete, :options, :patch]
 
 shared_examples_for "it routes '/' correctly" do |request_type|
   #run the request_type thru our example Sinatra app
@@ -12,15 +11,18 @@ shared_examples_for "it routes '/' correctly" do |request_type|
     end
   end
 
-  let(:response){ send(request_type, '/') }
+  let(:response){ request "/", :method => request_type.to_s.upcase }
   it("returns 201 as a status"){ expect(response.status).to be == 201 }
   it("sets the header as foo"){ expect(response.header['Header']).to be == 'foo' }
   it("returns the complete body as string"){ expect(response.body).to be == 'abc' }
 end
 
+request_types = [:post, :put, :delete, :options, :patch, :link, :unlink]
 request_types.each do |request_type|
   describe "#{request_type.upcase} request " do
     it_should_behave_like "it routes '/' correctly", request_type
   end
 end
+
+
 
