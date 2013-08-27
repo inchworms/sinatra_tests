d3f33ae4253971fb23534109e0f9447d4d740f66
carladrago <carladrago@gmail.com>
2013-08-27 11:24:32 +0200

    add basic_verb_route_spec.rb

---
+++ spec/basic_verb_routing_spec.rb
@@ -0,0 +1,26 @@
+# 'encoding: utf-8
+
+require 'spec_helper'
+
+request_types = [:post, :put, :delete, :options, :patch]
+
+shared_examples_for "it routes '/' correctly" do |request_type|
+  #run the request_type thru our example Sinatra app
+  let(:app) do
+    Sinatra.new do
+      send(request_type, '/'){ [201, { 'Header' => 'foo' }, ["a", "b", "c"]] }
+    end
+  end
+
+  let(:response){ send(request_type, '/') }
+  it("returns 201 as a status"){ expect(response.status).to be == 201 }
+  it("sets the header as foo"){ expect(response.header['Header']).to be == 'foo' }
+  it("returns the complete body as string"){ expect(response.body).to be == 'abc' }
+end
+
+request_types.each do |request_type|
+  describe "#{request_type.upcase} request " do
+    it_should_behave_like "it routes '/' correctly", request_type
+  end
+end
+
